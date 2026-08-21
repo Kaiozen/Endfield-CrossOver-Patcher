421607d2dcf6e3754c14d3923fe4a719ea928506
rakko <rakkotashi@gmail.com>
2026-08-21 00:59:56 -0400

    Fix Swift 6 Menu Helper process launch

--- Sources/EndfieldMenuHelper/main.swift
+++ Sources/EndfieldMenuHelper/main.swift
@@ -6,5 +6,19 @@ let payload = home
     + "/Library/Application Support/CrossOver/Bottles/Arknights Endfield"
     + "/.endfield-r11-runtime/launch-gryphlink-private-r11.sh"
 
-execl("/bin/sh", "sh", payload, nil)
-exit(127)
+let shell = Process()
+shell.executableURL = URL(fileURLWithPath: "/bin/sh")
+shell.arguments = [payload]
+
+do {
+    try shell.run()
+    shell.waitUntilExit()
+    exit(shell.terminationStatus)
+} catch {
+    let message =
+        "EndfieldMenuHelper could not start the private Endfield launcher: "
+        + error.localizedDescription
+        + "\n"
+    FileHandle.standardError.write(Data(message.utf8))
+    exit(127)
+}
